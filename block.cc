a2e94e031c2b48bde2e930e8733c2aafd9981f66
Grace Yin <graceyin0728@gmail.com>
2024-11-21 16:00:14 -0500

    added code to player and block

--- block.cc
+++ block.cc
@@ -1,5 +1,5 @@
 #include "block.h"
 #include <iostream>
 
-BlockDecorator::BlockDecorator(Board* base) : base(base) {}
-BlockDecorator::~BlockDecorator() { delete base; }
+Block::Block(Board* base) : base(base) {}
+Block::~Block() { delete base; }
