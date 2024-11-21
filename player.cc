d603a5734b0f4dc6cb860ba8de8d5bae15bef0ff
Grace Yin <graceyin0728@gmail.com>
2024-11-21 13:44:07 -0500

    adding player fields and text fields

--- player.cc
+++ player.cc
@@ -1,9 +1,30 @@
 #include "player.h"
 
-Player::Player(Board* canvas): canvas{canvas}
+Player::Player(Board *canvas, int score, int high, Level *Level) : canvas{canvas}, score{score}, highscore{high}, level{level}
 {
+    Studio s{canvas};
 }
 
 Player::~Player()
 {
+    delete Player;
+}
+
+int Player::getScore(){
+    return score;
+}
+int Player::getHighScore(){
+    return highscore;
+}
+Level *Player::getLevel(){
+    return level;
+}
+Level *Player::Levelup(){
+    // level up
+}
+Level *Player::Leveldown(){
+    // level down
+}
+void Player::force(){
+    // force
 }
